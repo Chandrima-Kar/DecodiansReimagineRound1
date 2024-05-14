5299a6d30cb60c3c4c72111081de73bc39fed862
Chandrima Kar <chandrimakar16@gmail.com>
2024-05-14 14:53:32 +0530

    second commit

---
+++ camel-website/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div><h1 className="bg-black text-7xl text-white font-extrabold">Pratik Biswas</h1></div>
+    </div>
+  );
+}
+
+export default App;
